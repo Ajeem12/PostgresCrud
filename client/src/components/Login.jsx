4f7e1d9488f1f478dd79802692080f99160b064d
Ajeem Beg <138427053+Ajeem12@users.noreply.github.com>
2024-11-21 11:03:25 +0530

    Update Login.jsx

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:3000/api/v1/auth/login", {
+      const res = await axios.post("https://postgrescrud-bakend.onrender.com/api/v1/auth/login", {
         email,
         password,
       });
